188b0891d51d2d57161fd01822ded7b9db019164
yzj <915038446@qq.com>
2021-07-22 14:44:52 +0800

    1

---
+++ src/test/java/com/hcsummercamp/fdkgateway/FdkGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcsummercamp.fdkgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdkGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
